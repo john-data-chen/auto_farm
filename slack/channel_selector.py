b890b0676ca6dd182f7f0b518b923748d79c6dde
john.data_chen <john.data_chen@ebc.net.tw>
2016-02-05 16:38:33 +0800

    [Doc] change to D.D team slack channels

--- slack/channel_selector.py
+++ slack/channel_selector.py
@@ -1,4 +1,3 @@
-import os
 from msg_sender import *
 from ConfigParser import SafeConfigParser
 
@@ -11,20 +10,12 @@ pass_to_formal_channel = config_parser.get('PM_VERIFIED_PASS', 'PASS_TO_FORMAL_C
 
 
 def select(target, text_to_slack):
-	# detect which folder
-	if os.getcwd() == "/home/charlie/public_html/production/Jarvis_alpha":
-		# after PM verified result is Pass, then send to formal channels
-		if target in targets_online:
-			if target in pass_to_formal_channel:
-				send_ebc_robot(text_to_slack)
-				send_ebc_important_info(text_to_slack)
-				send_ebc_pm(text_to_slack)
-			else:
-				send_ebcinsight_cart(text_to_slack)
+	# after PM verified result is Pass, then send to formal channels
+	if target in targets_online:
+		if target in pass_to_formal_channel:
+			send_auto_farm(text_to_slack)
 		else:
-			# send to test channels
-			send_ebcinsight_cart(text_to_slack)
-			send_ebc_pm_test(text_to_slack)
+			send_crawler_test(text_to_slack)
 	else:
-		# send to test channel: crawler
-		send_crawler(text_to_slack)
+		# send to test channel
+		send_crawler_test(text_to_slack)
--- slack/msg_sender.py
+++ slack/msg_sender.py
@@ -7,80 +7,47 @@ config_parser = SafeConfigParser()
 config_parser.read('./config.txt')
 
 # channels
-# error warning only
-rd_team = config_parser.get('SLACK', 'RD_TEAM')
-# test channel in EBC-TV
-crawler = config_parser.get('SLACK', 'CRAWLER')
-# test channel in EBC
-ebcinsight_cart = config_parser.get('SLACK', 'EBCINSIGHT-CART')
-# formal channel in EBC
-ebc_robot = config_parser.get('SLACK', 'EBC_ROBOT')
-ebc_important_info = config_parser.get('SLACK', 'EBC_IMPORTANT_INFO')
-# formal channel for PM
-ebc_pm = config_parser.get('SLACK', 'EBC_PM')
-# test channel for PM
-ebc_pm_test = config_parser.get('SLACK', 'EBC_PM_TEST')
+# error warning and testing
+crawler_test = config_parser.get('SLACK', 'CRAWLER_TEST')
+# formal release channel
+auto_farm = config_parser.get('SLACK', 'AUTO_FARM')
 
 
 def temp_missing(temp_name):
-	slack_webhook = rd_team
+	slack_webhook = crawler_test
 	payload = {"text": "template " + temp_name + ".json does not exist, exit!"}
 	requests.post(slack_webhook, json=payload)
 	exit()
 
 
 def crawl_page_error(url):
-	slack_webhook = rd_team
+	slack_webhook = crawler_test
 	payload = {"text": "crawl page: " + url + " failed, exit!"}
 	requests.post(slack_webhook, json=payload)
 	exit()
 
 
 def assign_values_error(error):
-	slack_webhook = rd_team
+	slack_webhook = crawler_test
 	payload = {"text": "assign values has exception: " + error}
 	requests.post(slack_webhook, json=payload)
 	exit()
 
 
 def get_tts_error(error):
-	slack_webhook = rd_team
+	slack_webhook = crawler_test
 	payload = {"text": error}
 	requests.post(slack_webhook, json=payload)
 	exit()
 
 
-def send_crawler(news):
-	slack_webhook = crawler
+def send_crawler_test(news):
+	slack_webhook = crawler_test
 	payload = {"text": news}
 	requests.post(slack_webhook, json=payload)
 
 
-def send_ebcinsight_cart(news):
-	slack_webhook = ebcinsight_cart
-	payload = {"text": news}
-	requests.post(slack_webhook, json=payload)
-
-
-def send_ebc_robot(news):
-	slack_webhook = ebc_robot
-	payload = {"text": news}
-	requests.post(slack_webhook, json=payload)
-
-
-def send_ebc_important_info(news):
-	slack_webhook = ebc_important_info
-	payload = {"text": news}
-	requests.post(slack_webhook, json=payload)
-
-
-def send_ebc_pm(news):
-	slack_webhook = ebc_pm
-	payload = {"text": news}
-	requests.post(slack_webhook, json=payload)
-
-
-def send_ebc_pm_test(news):
-	slack_webhook = ebc_pm_test
+def send_auto_farm(news):
+	slack_webhook = auto_farm
 	payload = {"text": news}
 	requests.post(slack_webhook, json=payload)
